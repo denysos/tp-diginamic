7dd678d17ed2e10b6ef218cb2c1476408d422317
Jonathan Guerino <guerino.jonathan@gmail.com>
2021-12-17 14:18:01 +0100

    Ajout partie JS

---
+++ cours-js/exemple-dom/js/tp02.js
@@ -0,0 +1,16 @@
+let num1 = 12;
+let num2 = 3;
+
+console.log("addition");
+console.log(num1+num2);
+
+console.log("soustraction");
+console.log(num1 - num2);
+
+console.log("multiplication");
+console.log(num1 * num2);
+
+
+if (num1 > num2){
+  console.log(`${num1} est superieur à ${num2}`)
+}
